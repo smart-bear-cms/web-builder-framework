a41c41cc615892d8ca85cbf8edf1d412d253cc7b
nguyenanhung <hungna.dev@gmail.com>
2025-01-19 23:29:47 +0700

    Update v1.0.6

---
+++ src/Version.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project web-builder-framework
+ * Created by PhpStorm
+ * User: 713uk13m <dev@nguyenanhung.com>
+ * Copyright: 713uk13m <dev@nguyenanhung.com>
+ * Date: 19/1/25
+ * Time: 23:28
+ */
+
+namespace nguyenanhung\Platforms\WebBuilderFramework\System;
+
+class Version
+{
+    const VERSION = '1.0.6';
+}
